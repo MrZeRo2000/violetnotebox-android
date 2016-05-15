17d635452eff2d425de41db2c0d1230f364c4c0e
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-05-15 11:27:17 +0300

    dropbox download code cleanup

--- app/src/main/java/com/romanpulov/violetnotebox/DropboxManagementActivity.java
+++ app/src/main/java/com/romanpulov/violetnotebox/DropboxManagementActivity.java
@@ -3,6 +3,7 @@ package com.romanpulov.violetnotebox;
 import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -14,7 +15,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.dropbox.core.DbxWebAuth;
+import com.dropbox.core.DbxDownloader;
 import com.dropbox.core.android.Auth;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.FileMetadata;
@@ -31,19 +32,23 @@ public class DropboxManagementActivity extends AppCompatActivity {
         Log.d("DMActivity", message);
     }
 
-
     private static final String SHARED_PREFERENCES_NAME = "dropbox-preferences";
     private static final String SHARED_PREFERENCES_ACCESS_TOKEN = "access-token";
     private static final String SHARED_PREFERENCES_REMOTE_PATH = "remote-path";
     private static final String SHARED_PREFERENCES_REMOTE_FILE_NAME = "remote-file-name";
+    private static final String LOCAL_FILE_NAME = "localfile";
+
+    private static final int STATUS_OK = 0;
+    private static final int STATUS_ERROR = 1;
 
     private SharedPreferences mPrefs;
     private String mAccessToken;
 
     private TextView mAccessTokenTextView;
+    private TextView mMessageTextView;
 
     private interface DropboxCallback {
-        void onDataLoaded(File file);
+        void onDataLoaded(FileMetadata fileMetadata);
         void onError(Exception e);
     }
 
@@ -54,6 +59,7 @@ public class DropboxManagementActivity extends AppCompatActivity {
 
         mPrefs = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE);
         mAccessToken = getAccessToken();
+        log("AccessToken=" + mAccessToken);
 
         initControls();
     }
@@ -70,6 +76,7 @@ public class DropboxManagementActivity extends AppCompatActivity {
             }
         });
 
+        /*
         Button connectButton = (Button)findViewById(R.id.connectButton);
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +88,7 @@ public class DropboxManagementActivity extends AppCompatActivity {
                 updateAccessTokenTextView();
             }
         });
+        */
 
         EditText remotePathEditText = (EditText)findViewById(R.id.remotePathEditText);
         remotePathEditText.setText(mPrefs.getString(SHARED_PREFERENCES_REMOTE_PATH, null));
@@ -120,7 +128,7 @@ public class DropboxManagementActivity extends AppCompatActivity {
             }
         });
 
-        final TextView messageTextView = (TextView)findViewById(R.id.messageTextView);
+        mMessageTextView = (TextView)findViewById(R.id.messageTextView);
 
         Button syncButton = (Button)findViewById(R.id.syncButton);
         syncButton.setOnClickListener(new View.OnClickListener() {
@@ -130,25 +138,59 @@ public class DropboxManagementActivity extends AppCompatActivity {
                 String remoteFileName = mPrefs.getString(SHARED_PREFERENCES_REMOTE_FILE_NAME, null);
                 log("sync: " + remotePath + ", " + remoteFileName);
 
+                mAccessToken = Auth.getOAuth2Token();
+                if (mAccessToken == null) {
+                    setMessage(STATUS_ERROR, getResources().getString(R.string.message_error_auth));
+                    return;
+                }
 
+                log("setting access token:" + mAccessToken);
+                setAccessToken(mAccessToken);
+                updateAccessTokenTextView();
+
+                log("init with access token:" + mAccessToken);
                 DropboxClientFactory.init(mAccessToken);
 
-                File f = new File(getCacheDir(), "file1");
+                File f = new File(getCacheDir(), LOCAL_FILE_NAME);
                 FileGetAsyncTask fileGetAsyncTask = new FileGetAsyncTask(DropboxClientFactory.getClient(), f, new DropboxCallback() {
                     @Override
-                    public void onDataLoaded(File file) {
-                        messageTextView.setText("Successfully loaded " + file.getName());
+                    public void onDataLoaded(FileMetadata fileMetadata) {
+                        if (fileMetadata.getSize() > 0)
+                            setMessage(STATUS_OK, fileMetadata.getName());
+                        else
+                            setMessage(STATUS_ERROR, getResources().getString(R.string.message_error_download_file_zero));
                     }
 
                     @Override
                     public void onError(Exception e) {
-                        messageTextView.setText(e.getMessage());
+                        setMessage(STATUS_ERROR, e.getMessage());
                     }
                 });
                 fileGetAsyncTask.execute(remotePath, remoteFileName);
             }
         });
 
+    }
+
+    private void setMessage(int status, String message) {
+        int messageColor;
+        String messageText;
+        switch (status) {
+            case STATUS_ERROR:
+                messageColor = ContextCompat.getColor(this, R.color.colorTextError);
+                messageText = getResources().getString(R.string.message_error_loading, message);
+                break;
+            case STATUS_OK:
+                messageColor = ContextCompat.getColor(this, R.color.colorTextMessage);
+                messageText = getResources().getString(R.string.message_successfully_loaded, message);
+                break;
+            default:
+                messageColor = 0;
+                messageText = null;
+        }
+
+        mMessageTextView.setTextColor(messageColor);
+        mMessageTextView.setText(messageText);
     }
 
     private String getAccessToken() {
@@ -167,7 +209,7 @@ public class DropboxManagementActivity extends AppCompatActivity {
             mAccessTokenTextView.setText(R.string.access_token_exists);
     }
 
-    private class FileGetAsyncTask extends AsyncTask<String, Void, File> {
+    private class FileGetAsyncTask extends AsyncTask<String, Void, FileMetadata> {
         private ProgressDialog mProgressDialog;
         private final DbxClientV2 mClient;
         private final File mFile;
@@ -177,22 +219,20 @@ public class DropboxManagementActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             mProgressDialog = new ProgressDialog(DropboxManagementActivity.this);
-            mProgressDialog.setTitle("Loading ...");
+            mProgressDialog.setTitle(getResources().getString(R.string.message_downloading_file_progress));
             mProgressDialog.show();
         }
 
         @Override
-        protected File doInBackground(String... params) {
+        protected FileMetadata doInBackground(String... params) {
             try {
-                //return mClient.files().listFolder(params[0]);
                 ListFolderResult folderResult = mClient.files().listFolder(params[0]);
                 for (Metadata e: folderResult.getEntries()) {
                     if ((e instanceof FileMetadata) && (e.getName().equals(params[1]))) {
                         FileMetadata f = (FileMetadata) e;
                         OutputStream outputStream = new FileOutputStream(mFile);
                         try  {
-                            mClient.files().download(e.getPathLower(), f.getRev()).download(outputStream);
-                            return mFile;
+                            return mClient.files().download(e.getPathLower(), f.getRev()).download(outputStream);
                         } finally {
                             outputStream.flush();
                             outputStream.close();
@@ -214,14 +254,14 @@ public class DropboxManagementActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(File file) {
-            super.onPostExecute(file);
+        protected void onPostExecute(FileMetadata fileMetadata) {
+            super.onPostExecute(fileMetadata);
             mProgressDialog.dismiss();
 
             if (mException != null) {
                 mCallback.onError(mException);
             } else {
-                mCallback.onDataLoaded(file);
+                mCallback.onDataLoaded(fileMetadata);
             }
         }
     }
